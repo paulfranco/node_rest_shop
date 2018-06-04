5933651662e239515d636ba8277d1814780a4d5b
Paul Franco <paulfranco@me.com>
2018-06-04 00:34:46 -0700

    added CORS Error handling in app.js

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// CORS Error Handling
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origi", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Routes which should handle the incoming requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
